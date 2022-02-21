69ff824db4b0656b3ea629455339ab4bbaa4bff5
alvaro-j <alvarojaime360@gmail.com>
2022-02-21 09:44:42 -0300

    add weather icon

--- src/components/WeatherLocation.jsx
+++ src/components/WeatherLocation.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 
-const WeatherLocation = ({weather}) => {
+const WeatherLocation = ({ weather }) => {
 	return (
 		<div className="location">
 			<h2 className="location__name">
-        <span>{weather.name}</span>
-        <sup>{weather.sys.country}</sup>
-      </h2>
+				<span>{weather.name}</span>
+				<sup>{weather.sys.country}</sup>
+			</h2>
 			<div className="location__temp">
 				{Math.round(weather.main.temp)}
 				<sup>&deg;C</sup>
+			</div>
+			<div className="location__icon">
+				<img
+					src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+					alt={weather.weather[0].description}
+				/>
 			</div>
 		</div>
 	);
