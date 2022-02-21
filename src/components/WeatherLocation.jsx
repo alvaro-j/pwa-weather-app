6da759d6f6a105c631fccd38ea2344f28366f50b
alvaro-j <alvarojaime360@gmail.com>
2022-02-21 09:36:59 -0300

    add location temperature div

--- src/components/WeatherLocation.jsx
+++ src/components/WeatherLocation.jsx
@@ -7,6 +7,10 @@ const WeatherLocation = ({weather}) => {
         <span>{weather.name}</span>
         <sup>{weather.sys.country}</sup>
       </h2>
+			<div className="location__temp">
+				{Math.round(weather.main.temp)}
+				<sup>&deg;C</sup>
+			</div>
 		</div>
 	);
 };
