526713cfe943305ae74c878043b03586bfea704a
alvaro-j <alvarojaime360@gmail.com>
2022-02-22 09:38:30 -0300

    install cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,2 +1,20 @@
 const CACHE_NAME = "version1";
 const urlsToCache = ["index.html", "offline.html"];
+
+const self = this; // in the service worker file, "this" represents the actual service worker
+
+// install
+self.addEventListener("install", (e) => {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log("opened cache");
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+// listen
+self.addEventListener("fetch", (e) => {});
+
+// activate
+self.addEventListener("activate", (e) => {});
